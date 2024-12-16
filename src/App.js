e2296a88b42be24201482b0c9f1214e30262b7b8
swapsideee <statixfaceit@gmail.com>
2024-12-16 17:01:01 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       {}
       <main>
       </main>
+      <Footer />
     </div>
   );
 };
